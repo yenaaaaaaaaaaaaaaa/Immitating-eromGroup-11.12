b4a36f24de2e6a6fbf48558f5f5011e9f7d00c2c
yenaaaaaaaaaaaaaaa <yeana7777@gmail.com>
2025-11-15 21:11:07 +0900

    헤더와 GNB 메뉴 구현 완료

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,11 @@
 console.clear();
 const headerUtilsLang = document.querySelector(".header-utils__lang");
-const langList = document.querySelector('.lang-list');
+const langList = document.querySelector(".lang-list");
+const menuBtn = document.querySelector(".header-utils__menu-btn .menu-btn");
+const gnbBox = document.querySelector(".gnb-box");
+/*
+header > dropdown language list
+*/
 function headerLangOption() {
   headerUtilsLang.addEventListener("click", function () {
     const isActive = this.classList.contains("active");
@@ -12,14 +17,34 @@ function headerLangOption() {
       addActiveClass(headerUtilsLang);
     }
   });
-  langList.addEventListener('click', function(e){
+  langList.addEventListener("click", function (e) {
     e.stopPropagation();
   });
 }
+/*
+add/remove active class
+*/
 function addActiveClass(el) {
   el.classList.add("active");
 }
 function removeActiveClass(el) {
   el.classList.remove("active");
 }
+
+/*
+header > menu-btn motion
+*/
+function menuBtnMotion() {
+  menuBtn.addEventListener("click", function () {
+    const isActive = this.classList.contains("active");
+    if (isActive) {
+      removeActiveClass(this);
+      removeActiveClass(gnbBox);
+    } else {
+      addActiveClass(this);
+      addActiveClass(gnbBox);
+    }
+  });
+}
 headerLangOption();
+menuBtnMotion();
